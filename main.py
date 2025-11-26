39bccd04a31dd56d4db1a85873a6173468e5b4e2
MIKAEL CARLOS RIBEIRO DE FRANCA <2023326189@MACEIO.IFAL>
2025-11-26 09:43:24 -0300

    firs commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+
+# stop mouse 
+pyautogui.FAILSAFE = True
+
+# open browser
+pyautogui.hotkey("win")
+pyautogui.write("Chrome", interval=0.1)
+pyautogui.press("enter")
+
+# search web
+
+pyautogui.moveTo(800,600, duration=1)
+pyautogui.click()
+pyautogui.write("wikipedia.org", interval=0.1)
+pyautogui.press("enter")
+
+# search on site
+time.sleep(2)
+pyautogui.write("Linus Torvald", interval=0.1)
+pyautogui.press("enter")
+time.sleep(2)
+pyautogui.moveTo(300,600, duration=1)
+pyautogui.click()
+time.sleep(2)
+
+# screenshot on page
+img = pyautogui.screenshot()
+img.save("picture.png")
